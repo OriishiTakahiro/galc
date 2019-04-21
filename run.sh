c5d84d62558385644a853bccdba29bab2df3e449
OriishiTakahiro <takahiro0914@live.jp>
2019-04-21 17:54:06 +0900

    Fix bug

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-GO_FILES=$(ls src | grep .go)
-echo "run from $GO_FILES"
+GO_FILES=$(ls src | grep .go | grep -v test)
+echo "run from \n $GO_FILES"
 docker run --rm -v ${PWD}/src:/go/src/galc -w /go/src/galc golang:1.12.3-alpine go run ${GO_FILES} $@
